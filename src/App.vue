fa3d03ec3156467c645a3969c25b4ddf13884580
amiceli <amiceli@letsignit.com>
2023-11-20 22:47:34 +0100

    chore: init src

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="App">
+        <RouterView />
+    </div>
+</template>
+
+<script setup lang="ts">
+    // 
+</script>
+
+<style lang="scss" scoped>
+    .App {}
+</style>
